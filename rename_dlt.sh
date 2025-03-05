2e9101b0fc28160b347980e9e0b71c035e4ed5a9
Egon Coppens <egon.coppens@sympower.net>
2025-03-05 22:27:40 +0000

    feat: add renaming script

---
+++ rename_dlt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ORIGINAL_NAME="dlt"
+NEW_NAME="data_load_tool"
+
+# Rename the package directory
+if [ -d "$ORIGINAL_NAME" ]; then
+    mv "$ORIGINAL_NAME" "$NEW_NAME"
+    echo "Renamed folder $ORIGINAL_NAME → $NEW_NAME"
+else
+    echo "Folder $ORIGINAL_NAME not found!"
+    exit 1
+fi
+
+# Update all dlt mentions besides words with "dlt" as substring surounded by "-" or "_"
+find . -type f ! -path "./.git/*" -exec perl -i -pe "s/(?<![-_])\b$ORIGINAL_NAME\b(?![-_])/$NEW_NAME/g" {} +
+echo "Updated all dlt mentions in files"
+
+# Rename ".dlt" hidden folders
+find . -type d -name ".$ORIGINAL_NAME" -execdir mv {} ".$NEW_NAME" \;
+echo "Renamed all .$ORIGINAL_NAME folders"
+
+echo "Done!⚡️ Package renamed"
